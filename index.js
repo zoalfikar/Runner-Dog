c72b2b98e21957af909f45a20a7867487c86e59e
zolafikar <zoalfikaralsaad@gmail.com>
2023-01-26 02:42:36 -0800

    //

--- index.js
+++ index.js
@@ -46,13 +46,17 @@ class Player{
         this.x = 0 ;
         this.y = this.gameHeight - this.height;
         this.image = document.getElementById('playerImage')
+        this.frameX = 0;
+        this.frameY = 0;
         this.speed = 0;
+        this.vy = 0;
+        this.weight =1;
     }
     draw(context){
         context.fillStyle = 'white';
         context.fillRect(this.x,this.y,this.width,this.height);
         // context.drawImage(this.image ,0 ,0 , this.width ,this.height ,this.x ,this.y , this.width ,this.height);
-        context.drawImage(this.image ,this.x ,this.y ,this.width,this.height);
+        context.drawImage(this.image,this.frameX*this.width,this.frameY*this.height,this.width,this.height ,this.x ,this.y ,this.width,this.height);
     }
     update(input){
         this.x += this.speed;
@@ -60,9 +64,33 @@ class Player{
             this.speed = 5;
         }
         else{
-            this.speed = 0;
+            if (input.keys.indexOf('ArrowLeft')>-1) {
+                this.speed = -5;
+            }
+            else{
+                this.speed = 0;
+            }
 
         }
+        if (input.keys.indexOf('ArrowUp')>-1 && this.onGround()) {
+            this.vy -= 32;
+        }
+     
+        
+        if(this.x < 0) this.x=0
+        else if(this.x > this.gameWidth - this.width) this.x =this.gameWidth-this.width
+
+        this.y += this.vy
+        if (!this.onGround()) {
+            this.vy += this.weight
+        }
+        else{
+            this.vy =0;
+        }
+        if(this.y > this.gameHeight - this.height) this.y=this.gameHeight - this.height
+    }
+    onGround(){
+        return this.y >= this.gameHeight - this.height
     }
     
 }
@@ -92,12 +120,4 @@ function animate() {
 }
 animate()
 })
-function calculateVolume(length) {
-    return function (breadth) {
-        return function (height) {
-            return length * breadth * height;
-        }
-    }
-}
-console.log(calculateVolume(4)(5)(6));
 
