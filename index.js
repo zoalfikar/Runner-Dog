5c9bea1ed72d908e57e21873eaf88cf16f96aa12
zolafikar <zoalfikaralsaad@gmail.com>
2023-01-26 01:36:59 -0800

    side Crolller game update1

--- index.js
+++ index.js
@@ -92,3 +92,12 @@ function animate() {
 }
 animate()
 })
+function calculateVolume(length) {
+    return function (breadth) {
+        return function (height) {
+            return length * breadth * height;
+        }
+    }
+}
+console.log(calculateVolume(4)(5)(6));
+
